84172e79388495f48a27f86479a63ed41c36ad75
connorjohnlind <connorjohnlind@gmail.com>
2018-02-20 10:32:55 -0800

    Updated Redirect URI's for Production

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -9,7 +9,7 @@ const stravaClick = () => {
   if (window.location.href.includes('localhost')) {
     redirectUrl += '&redirect_uri=http://localhost:8080/';
   } else {
-    redirectUrl += '&redirect_uri=http://connorlind.com';
+    redirectUrl += '&redirect_uri=https://stravadash.herokuapp.com/';
   }
   window.location.replace(redirectUrl);
 };
