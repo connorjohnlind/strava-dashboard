f5ebbd495a8078be9653ae6e5fd5b756afcfd748
connorjohnlind <connorjohnlind@gmail.com>
2018-03-06 14:39:17 -0800

    Completed demo.js script

--- scripts/demo.js
+++ scripts/demo.js
@@ -1,17 +1,19 @@
+require('../config/config');
+
 const { MongoClient } = require('mongodb');
 const assert = require('assert');
 
-// Connection URL
 const url = 'mongodb://localhost:27017/StravaDash-demo';
-
-// Database Name
 const dbName = 'StravaDash-demo';
 
+const dummy = require('./demo.data');
+
 const insertDocuments = (db, callback) => {
-  // Get the documents collection
   const collection = db.collection('demoData');
-  // Insert some documents
-  collection.insert({ a: 1 }, (err, result) => {
+  collection.drop(() => {
+    console.log('Wiped collection');
+  });
+  collection.insert({ data: JSON.stringify(dummy) }, (err, result) => {
     assert.equal(err, null);
     assert.equal(1, result.result.n);
     assert.equal(1, result.ops.length);
