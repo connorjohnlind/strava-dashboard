e4136b0f0b812c600abd3e3c0d1e1af1bec5fdcc
connorjohnlind <connorjohnlind@gmail.com>
2018-03-26 15:35:16 -0700

    Added Icons to Calendar

--- client/components/Dashboard/Calendar/Day/Day.jsx
+++ client/components/Dashboard/Calendar/Day/Day.jsx
@@ -1,24 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import dateFns from 'date-fns';
+import Aux from '../../../hoc/Aux';
 
 import classes from './Day.scss';
 
 const Day = (props) => {
-  let activities;
+  let content;
   if (props.activities) {
-    activities = props.activities.map(activity => (
-      <p key={`${activity}_${Math.random()}`}>{activity}</p>
-    ));
-  }
-  return (
-    <div className={classes.content}>
-      <p>
+    const firstActivity = props.activities[0];
+    content = (
+      <Aux>
+        <p className={classes.hideDay}>
+          { props.firstOfMonth ? dateFns.format(props.date, 'MMM DD') : dateFns.format(props.date, 'D') }
+        </p>
+        <div className={classes[firstActivity]} />
+      </Aux>
+    );
+  } else {
+    content = (
+      <p className={classes}>
         { props.firstOfMonth ? dateFns.format(props.date, 'MMM DD') : dateFns.format(props.date, 'D') }
       </p>
-      {activities}
+    );
+  };
+
+  return (
+    <div className={classes.content}>
+      {content}
     </div>
-  );
+  )
 };
 
 Day.propTypes = {
